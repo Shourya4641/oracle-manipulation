795fb333f294185c95f9b871a9483ed771de5e81
Shourya4641 <shouryasarkar001@gmail.com>
2026-09-21 18:59:58 +0530

    feat: adds a naive lending protocol contract

---
+++ src/LendingVault.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "./MockERC20.sol";
+import "./NaiveSpotOracle.sol";
+
+contract LendingVault {
+    MockERC20 public coll;
+    MockERC20 public quote;
+    NaiveSpotOracle public oracle;
+    mapping(address => uint256) public collateralOf;
+    mapping(address => uint256) public debtOf;
+    uint256 public constant LTV = 80; // borrow up to 80% of collateral value
+
+    constructor(MockERC20 _c, MockERC20 _q, NaiveSpotOracle _o) {
+        coll = _c;
+        quote = _q;
+        oracle = _o;
+    }
+
+    function deposit(uint256 amount) external {
+        coll.transferFrom(msg.sender, address(this), amount);
+        collateralOf[msg.sender] += amount;
+    }
+    function borrow(uint256 amount) external {
+        uint256 price = oracle.priceOfColl(); // <-- trusts movable price
+        uint256 value = (collateralOf[msg.sender] * price) / 1e18;
+        uint256 maxDebt = (value * LTV) / 100;
+        require(debtOf[msg.sender] + amount <= maxDebt, "undercollateralized");
+        debtOf[msg.sender] += amount;
+        quote.transfer(msg.sender, amount);
+    }
+}
